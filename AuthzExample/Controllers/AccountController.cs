2bec24aac8787ccb8d182412fd4256e61ab37fab
Lee Brandt <leebrandt@gmail.com>
2018-03-21 14:21:35 -0500

    Initial Commit

---
+++ AuthzExample/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthzExample.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            if (!HttpContext.User.Identity.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+                return new HttpUnauthorizedResult();
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.SignOut(
+                    CookieAuthenticationDefaults.AuthenticationType,
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult PostLogout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize]
+        public ActionResult Profile() => View();
+
+        [Authorize(Roles = "Enthusiasts")]
+        public ActionResult Enthusiast() => View();
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult Admin() => View();
+    }
+}
---
+++ AuthzExample/Startup.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+
+[assembly: OwinStartup(typeof(AuthzExample.Startup))]
+
+namespace AuthzExample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+        private void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = CookieAuthenticationDefaults.AuthenticationType,
+            });
+
+            var clientId = ConfigurationManager.AppSettings["okta:ClientId"].ToString();
+            var clientSecret = ConfigurationManager.AppSettings["okta:ClientSecret"].ToString();
+            var issuer = ConfigurationManager.AppSettings["okta:Issuer"].ToString();
+            var redirectUri = ConfigurationManager.AppSettings["okta:RedirectUri"].ToString();
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                ClientId = clientId,
+                ClientSecret = clientSecret,
+                Authority = issuer,
+                RedirectUri = redirectUri,
+                ResponseType = "code id_token",
+                UseTokenLifetime = false,
+                Scope = "openid profile",
+                PostLogoutRedirectUri = ConfigurationManager.AppSettings["okta:PostLogoutRedirectUri"].ToString(),
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name"
+                },
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    RedirectToIdentityProvider = context =>
+                    {
+                        if (context.ProtocolMessage.RequestType == OpenIdConnectRequestType.Logout)
+                        {
+                            var idToken = context.OwinContext.Authentication.User.Claims
+                                .FirstOrDefault(c => c.Type == "id_token")?.Value;
+                            context.ProtocolMessage.IdTokenHint = idToken;
+                        }
+
+                        return Task.FromResult(true);
+                    },
+                    AuthorizationCodeReceived = async context =>
+                    {
+                        // Exchange code for access and ID tokens
+                        var tokenClient = new TokenClient(
+                            issuer + "/v1/token", clientId, clientSecret);
+                        var tokenResponse = await tokenClient.RequestAuthorizationCodeAsync(
+                            context.ProtocolMessage.Code, redirectUri);
+
+                        if (tokenResponse.IsError)
+                        {
+                            throw new Exception(tokenResponse.Error);
+                        }
+
+                        var userInfoClient = new UserInfoClient(issuer + "/v1/userinfo");
+                        var userInfoResponse = await userInfoClient.GetAsync(tokenResponse.AccessToken);
+
+                        var identity = new ClaimsIdentity();
+                        identity.AddClaims(userInfoResponse.Claims);
+
+                        identity.AddClaim(new Claim("id_token", tokenResponse.IdentityToken));
+                        identity.AddClaim(new Claim("access_token", tokenResponse.AccessToken));
+                        if (!string.IsNullOrEmpty(tokenResponse.RefreshToken))
+                        {
+                            identity.AddClaim(new Claim("refresh_token", tokenResponse.RefreshToken));
+                        }
+
+                        var nameClaim = new Claim(
+                            ClaimTypes.Name,
+                            userInfoResponse.Claims.FirstOrDefault(c => c.Type == "name")?.Value);
+                        identity.AddClaim(nameClaim);
+
+                        foreach(var group in userInfoResponse.Claims.Where(x => x.Type == "groups"))
+                        {
+                            identity.AddClaim(new Claim(ClaimTypes.Role, group.Value));
+                        }
+
+                        context.AuthenticationTicket = new AuthenticationTicket(
+                            new ClaimsIdentity(identity.Claims, context.AuthenticationTicket.Identity.AuthenticationType),
+                            context.AuthenticationTicket.Properties);
+                    }
+                }
+            });
+        }
+    }
+}
